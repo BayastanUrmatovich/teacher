f0992ee183f6c27826960f0e929108f13d781eed
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-06 14:59:34 +0600

    added node sass

--- src/components/Example3/Example3.js
+++ src/components/Example3/Example3.js
@@ -1,12 +1,27 @@
-// import classes from "./Example3.module.scss";
+import classes from "./Example3.module.scss";
+
+import { useState } from "react";
 
 
 
 function Example3() {
+
+  const [number, setNumber] = useState(0);
+  function onClick(event) {
+    setNumber(number+1);
+  }
+
   return ( 
     <>
-      <div className="Example3">
-        dsfad
+      <div className={classes.Example3}>
+
+        <div >
+          {number}
+        </div>
+        <button onClick={onClick}>
+          Plus
+        </button>
+        
       </div>
     </>
     );
